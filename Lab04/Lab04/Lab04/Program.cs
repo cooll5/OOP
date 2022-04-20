f1be869fc863a63c5ec55a56e7b2ea086462492e
cooll5 <52448498+cooll5@users.noreply.github.com>
2022-04-21 00:32:15 +0300

    Add files via upload

---
+++ Lab04/Lab04/Lab04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter c = new Counter();
+            c.Read();
+            Console.Write("Your output is:");
+            c.Display();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab04/Lab04/Lab04/Сounter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04
+{
+    public class Counter
+    {
+
+        private int sense;
+
+        public int GetSense() { return sense; }
+
+        public bool SetSense(int value)
+        {
+            if (value >= 0 && value < 100000) { sense = value; return true; }
+            else Console.WriteLine("Try to write your value again!");
+            { sense = 0; return false; }
+            
+        }
+
+        public bool Init(int s)
+        {
+            return SetSense(s);
+        }
+
+        public void Read()
+        {
+            int s;
+
+            do
+            {
+                Console.Write("Sense: ");
+                s = Convert.ToInt32(Console.ReadLine());
+
+            } while (!Init(s));
+        }
+        public void Display()
+        {
+            if (sense == 0)
+                Console.Write($" {sense} - you have no value");
+            else
+                Console.Write($" {sense} that is your current value");
+
+        }
+    }
+}
+
+    
+
+
