86d4cd56254c60e2a8a6ddd5e8cda342e3dfffde
cooll5 <52448498+cooll5@users.noreply.github.com>
2022-05-04 23:15:31 +0300

    Add files via upload

---
+++ Lab03_1/Lab 3.1/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3._1
+{
+    class Program
+    {
+        static int Countw(string str)
+        {
+            if (str.Length < 1)
+            {
+                return 0;
+            }
+            int b = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a')
+                {
+                    b++;
+                }
+            }
+            return b;
+        }
+        static int Counth(string str)
+        {
+            if (str.Length < 1)
+            {
+                return 0;
+            }
+            int a = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'b')
+                {
+                    a++;
+                }
+            }
+            return a;
+        }
+        static int Counte(string str)
+        {
+            if (str.Length < 1)
+            {
+                return 0;
+            }
+            int c = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'c')
+                {
+                    c++;
+                }
+            }
+            return c;
+        }
+        static string Change(string str)
+        {
+            string stringer;
+            if (str.Length < 1)
+            {
+                return str;
+            }
+            else
+            {
+                stringer = str.Replace("abc", "**");
+            }
+            return stringer;
+        }
+
+       
+        static void Main(string[] args)
+        {
+            string str;Console.WriteLine("Task 1");
+            Console.WriteLine("===========================================");
+
+            Console.WriteLine("Enter string:");
+            str = Console.ReadLine();
+            string str_modified;
+            str_modified = Change(str);
+
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine($"String: {str}");
+            Console.WriteLine($"New string: {str_modified}");
+            Console.WriteLine("===========================================");
+
+            Console.WriteLine("Task 2");
+            
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a')
+                {
+                    count++;
+                }
+            }
+            {
+                Console.WriteLine("The reminds of 'a' in the context:" + count);
+                Console.WriteLine("===========================================");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab03_2.3/Lab03_3/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace lab3
+{
+    class Program
+    {
+        public struct Student
+        {
+            public string prizv;
+            public int kurs;
+            public string spec;
+            public int fiz;
+            public int mat;
+            public int prog;
+        };
+
+        static void Main(string[] args)
+        {
+            string str = "Ukraine, (Drohobych), Lviv, University";
+            char[] modifiedstr = str.ToCharArray();
+            int k = 0;
+            Console.WriteLine();
+            Console.WriteLine($"Modified String = {String.Join("", modifiedstr)}");
+            modifiedstr = str.ToCharArray();
+
+            #region Task 2
+            Console.WriteLine();
+            Console.WriteLine("Task 2:");
+
+            int k1 = 0;
+            int k2 = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '(' && k != 2)
+                {
+                    k1++;
+                }
+
+                else if (k == 2)
+                    for (int j = 0; j < str.Length; j++)
+                    {
+                        if (str[i] == ')' && k != 2)
+                        {
+                            k2++;
+                        }
+                    }
+            }
+            if (k1 != 1 && k2 != 1)
+                Console.WriteLine("ERROR \n STRING CONTAINES MORE THAN 1 BRACKETS");
+            else
+            {
+                int pos = str.IndexOf("(");
+                int pos2 = str.IndexOf(")");
+                modifiedstr = str.Remove(pos, pos2 - pos + 1).ToCharArray();
+                Console.WriteLine($"Modified String = {String.Join("", modifiedstr)}");
+                modifiedstr = str.ToCharArray();
+            }
+
+            #endregion
+
+            #region Task 3
+            string[] lastnames = { "Kobyrynka", "Kotyk", "Klimasevskiy", "Yachechak", "Savshak", "Bilionok", "Senyshyn" };
+            string[] specialnist = { "Комп науки", "матем. i ек.", "iнформатика", "фiзика i iнф.", "трудове" };
+
+            Console.WriteLine();
+            Console.WriteLine("Task 3:");
+
+            Console.Write("Кiлькiсть студентiв = ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Student[] p = new Student[N]; // створення посилання на об`єкт типу Student
+            Random rand = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                p[i].prizv = lastnames[rand.Next(7)];
+                p[i].kurs = rand.Next(1, 5);
+                p[i].spec = specialnist[rand.Next(5)];
+                p[i].fiz = rand.Next(2, 6);
+                p[i].mat = rand.Next(2, 6);
+                p[i].prog = rand.Next(2, 6);
+            }
+            Console.WriteLine("===============================================================================\n" +
+                              "| № |  Прiзвище  | Курс | Cпецiальнiсть | Фiзика | Математика | Програмування |\n" +
+                              "-------------------------------------------------------------------------------");
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine($"|{i,3}|{p[i].prizv,12}|{p[i].kurs,6}|{p[i].spec,15}|{p[i].fiz,8}|{p[i].mat,12}|{p[i].prog,15}|");
+            }
+            Console.WriteLine("===============================================================================");
+
+            Console.WriteLine("Студенти що мають 5 з фiзики\n" +
+                          "| № |  Прiзвище  |");
+            for (int i = 0; i < N; i++)
+            {
+                if (p[i].fiz == 5)
+                {
+                    Console.WriteLine($"|{i,3}|{p[i].prizv,12}|");
+                }
+            }
+
+            #endregion
+        }
+    }
+}
