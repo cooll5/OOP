df57c11c44ecd5d6bbc8726bd14337f050a9ba24
cooll5 <52448498+cooll5@users.noreply.github.com>
2022-06-07 00:31:31 +0300

    C#

---
+++ Lab08/lab8/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab08
+{
+    internal class Circle : Point
+    {
+        private double radius;
+        public Circle() { this.SetX(0); this.SetY(0); this.SetR(1); }
+
+        public Circle(double x, double y, double r) { this.SetX(x); this.SetY(y); this.SetR(r); }
+
+        public void SetR(double value)
+        {
+            if (value > 0)
+                this.radius = value;
+            else
+                this.radius = 1;
+        }
+
+        public double Square()
+        {
+            return Math.Round(Math.PI * Math.Pow(radius, 2), 2);
+        }
+
+        public void CircleDisplay()
+        {
+            Console.WriteLine($"Circle's center is on O ({GetX()};{GetY()}) and r = {radius}\nSquare = {Square()}");
+        }
+
+    }
+}
---
+++ Lab08/lab8/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Lab08
+{
+    internal class Program
+    {
+        public static void Main(String[] args)
+        {
+            Circle circle = new Circle(1, 5, 9);
+            Elipse elipse = new Elipse(1, 1, 8, 5);
+            circle.CircleDisplay();
+            Console.WriteLine("----------------------------------------------");
+            elipse.ElipseDisplay();
+        }
+    }
+}
